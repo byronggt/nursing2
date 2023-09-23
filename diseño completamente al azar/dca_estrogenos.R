5a7f9e62fbd2d64883a3136654caa0a3238b4651
ByronG <byrong.gt@gmail.com>
2023-09-23 12:10:47 -0600

    Actualizado DCA

--- diseño completamente al azar/dca_estrogenos.R
+++ diseño completamente al azar/dca_estrogenos.R
@@ -1,16 +1,19 @@
 # Dr. Byron González
 # http://www.byrong.tk
 
-# Comparación de niveles de estrogen en tres grupos de mujeres
+# Comparación de niveles de estrogeno en tres grupos de mujeres
 
 # Colocar en memoria las bibliotecas a emplear
 if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(agricolae)){install.packages("agricolae")}
+if(!require(performance)){install.packages("performance")}
 if(!require(readxl)){install.packages("readxl")}
 
 # Importar el archivo "estrogenos.xlsx"
 estrogenos<-read_excel("estrogenos.xlsx")
 head(estrogenos)
 attach(estrogenos)
+edad<-as.factor(edad)
 
 # Graficar el comportamiento de cada grupo
 boxplot(e4~edad)
@@ -22,5 +25,18 @@ ggplot(estrogenos, aes(x=as.factor(edad),y=e4)) +
   xlab("Edad en años") +
   ylab("Nivel de estrógeno en pg/mt")
   
+# Solicitar el análisis de la varianza
+aov1<-lm(e4~edad)
+anova(aov1)
+
+# Revisar los supuestos del Andeva
+plot(aov1,1)
+plot(aov1,2)
+windows(10,10)
+check_model(aov1)
+check_normality(aov1)
+
+# Solicitar la prueba múltiple de medias bajo el criterio de Tukey
+
 
 
