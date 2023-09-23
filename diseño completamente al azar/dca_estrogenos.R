11f595bb72ffb357753d233046aa436e38e6e3d7
ByronG <byrong.gt@gmail.com>
2023-09-23 11:49:09 -0600

    Actualizado DCA

--- diseño completamente al azar/dca_estrogenos.R
+++ diseño completamente al azar/dca_estrogenos.R
@@ -4,4 +4,18 @@
 # Comparación de niveles de estrogen en tres grupos de mujeres
 
 # Colocar en memoria las bibliotecas a emplear
+if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(readxl)){install.packages("readxl")}
+
+# Importar el archivo "estrogenos.xlsx"
+estrogenos<-read_excel("estrogenos.xlsx")
+head(estrogenos)
+attach(estrogenos)
+
+# Graficar el comportamiento de cada grupo
+boxplot(e4~edad)
+
+# Graficar el comportamiento mediante ggplot
+
+
 
