b3c88dbb71d1a06370e3a9eb6943c9553bd1c7bd
ByronG <byrong.gt@gmail.com>
2023-10-28 16:33:49 -0600

    Actualizado DCA para estrógenos

--- diseño completamente al azar/dca_estrogenos.R
+++ diseño completamente al azar/dca_estrogenos.R
@@ -8,6 +8,9 @@ if(!require(ggplot2)){install.packages("ggplot2")}
 if(!require(agricolae)){install.packages("agricolae")}
 if(!require(performance)){install.packages("performance")}
 if(!require(readxl)){install.packages("readxl")}
+if(!require(dplyr)){install.packages("dplyr")}
+if(!require(multcompView)){install.packages("multcompView")}
+
 
 # Importar el archivo "estrogenos.xlsx"
 estrogenos<-read_excel("estrogenos.xlsx")
@@ -39,7 +42,7 @@ estrogenos$residuos<-model1$residuals
 head(estrogenos)
 tail(estrogenos)
 andeva<-aov(e4~edad)
-
+summary(andeva)
 
 # Revisar los supuestos del Andeva
 plot(model1,1)
@@ -50,3 +53,30 @@ check_normality(model1)
 
 # Solicitar la prueba múltiple de medias bajo el criterio de Tukey
 pr.medias<-HSD.test(andeva, "edad", console = T) ; pr.medias
+
+# Procedimiento alternativo para Tukey
+tukey<-TukeyHSD(andeva); tukey
+cld<-multcompLetters4(andeva,tukey); cld
+
+# Tabla con la media
+Tk <- group_by(estrogenos, edad) %>%
+  summarise(mean=mean(e4)) %>%
+  arrange(desc(mean))
+
+# Extraer las letras y agregarlas a la tabla Tk
+cld <- as.data.frame.list(cld$edad)
+Tk$cld <- cld$Letters
+
+print(Tk)
+
+# Box plot con letras (Revisar la leyenda)
+
+ggplot(estrogenos, aes(x=as.factor(edad), y=e4)) + 
+  geom_boxplot(aes(fill=as.factor(edad))) +
+  labs(x="Edad", y="Nivel de estrógenos en pg/ml") +
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  geom_text(data = Tk, aes(as.factor(edad), y = mean, label = cld), size = 3, vjust=-1, hjust =-1)
+
+# Guardar el box plot
+ggsave("boxplot.png", width = 4, height = 3, dpi = 1000)
